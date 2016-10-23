c702c1ed17bf18cd3ee6b27c9982b32719d55147
Rocker2503 <npradenas.vargas@gmail.com>
2016-10-22 23:26:54 -0300

    Or parseado con split

--- Tarea1.java
+++ Tarea1.java
@@ -4,5 +4,10 @@ public class Tarea1
 {
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		String regex = sc.nextLine();
+		String text = sc.nextLine();
+
+		String[] parseReg = regex.split("|");	
 	}
 }
