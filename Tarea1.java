b52d290f062dfdf2b27ea4c1b74dd66fa55b01a4
juanIgnacioC <jcorderuiz@gmail.com>
2016-10-22 23:48:37 -0300

    split []

--- Tarea1.java
+++ Tarea1.java
@@ -5,9 +5,17 @@ public class Tarea1
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+                System.out.println("Ingrese la expresion regular: ");
 		String regex = sc.nextLine();
+                System.out.println("Ingrese el texto: ");
 		String text = sc.nextLine();
 
-		String[] parseReg = regex.split("|");	
+		String[] parseReg = regex.split("[|]");
+                
+                for (int i = 0; i < parseReg.length; i++)
+            {
+                System.out.println("interno: " + parseReg[i]);
+                
+            }
 	}
 }
