53e8fa8ca3ef4576de9645ac670266cec809389f
Rocker2503 <npradenas.vargas@gmail.com>
2016-10-23 00:23:50 -0300

    cochinada para usar recursividad

--- Tarea1.java
+++ Tarea1.java
@@ -3,19 +3,23 @@ import java.util.*;
 public class Tarea1
 {
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-                System.out.println("Ingrese la expresion regular: ");
+		new Main();
+    }
+
+    public Main()
+    {
+        Scanner sc = new Scanner(System.in);
+                //System.out.println("Ingrese la expresion regular: ");
 		String regex = sc.nextLine();
-                System.out.println("Ingrese el texto: ");
+                //System.out.println("Ingrese el texto: ");
 		String text = sc.nextLine();
 
 		String[] parseReg = regex.split("[|]");
                 
-                for (int i = 0; i < parseReg.length; i++)
-            {
-                System.out.println("interno: " + parseReg[i]);
-                
-            }
-	}
+        for (int  i = 0; i < parseReg.length; i++) 
+        {
+        		
+        }
+    }
+	
 }
