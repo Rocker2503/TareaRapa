914ac8711714c6fccae4ad40f94a322e4812bf90
Rocker2503 <npradenas.vargas@gmail.com>
2016-10-23 01:41:38 -0300

    esqueleto de la tarea definido

--- Tarea1.java
+++ Tarea1.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Tarea1
 {
 	public static void main(String[] args) {
-		new Main();
+		new Tarea1();
     }
 
     public Tarea1()
@@ -14,12 +14,49 @@ public class Tarea1
                 //System.out.println("Ingrese el texto: ");
 		String text = sc.nextLine();
 
-		String[] parseReg = regex.split("[|]");
-                
-        for (int  i = 0; i < parseReg.length; i++) 
-        {
-        		
-        }
+		//hacer un split para quitar los | 
+
+		ArrayList<Character> sigma = new ArrayList<>();
+		ArrayList<String> states = new ArrayList<>();
+		ArrayList<Trans> trans = new ArrayList<>();
+
+		String q = "q0";
+		int cont = 1;
+
+		states.add(q);
+
+		char[] characters = regex.toCharArray();
+		for (int i = 0; i < characters.length ; i++) {
+			if(characters[i] == '.' || characters[i] == '*')
+			{
+				if(characters[i] == '.')
+				{
+
+				}
+				else if(characters[i] == '*')
+				{
+
+				}
+			}
+			else
+			{
+				if(!sigma.contains(characters[i])){sigma.add(characters[i]);}
+				else{continue;}
+			}
+		}
     }
 	
+}
+
+class Trans
+{
+	String start, end;
+	char character;
+
+	public Trans(String s, String e, char c)
+	{
+		this.start = s;
+		this.character = c;
+		this.end = e;
+	} 
 }
